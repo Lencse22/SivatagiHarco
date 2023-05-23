333998cd5a332671ce039e2fd77ad0cef2ab4cf5
Lencse22 <marcell.lencses@stud.uni-corvinus.hu>
2023-05-23 20:35:18 +0200

    kereső előrehaladás

---
+++ SivatagiHarco/Models/MentettKeresés.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RFProjekt.Dnn.KeresoModulSivatagiHarco.Models
+{
+    public class MentettKeresés
+    {
+        public int ID { get; set; }
+        public string User { get; set; }
+        public string Név { get; set; }
+        public string Kategória { get; set; }
+        public string Terméktípús { get; set; }
+        public string Nem { get; set; }
+
+    }
+}
